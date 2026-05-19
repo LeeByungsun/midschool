fdb4b188793b0666e27022df7ae303a02339e509
bslee <buggyani@hanmail.net>
2026-05-19 15:24:24 +0900

    task: 홈/설정 동기화 회귀 방지 테스트 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,6 +50,11 @@ android {
         buildConfig = true
         dataBinding = true
     }
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
 }
 
 dependencies {
@@ -69,6 +74,7 @@ dependencies {
     ksp(libs.hilt.compiler)
     testImplementation(libs.junit)
     testImplementation(libs.robolectric)
+    testImplementation(libs.androidx.test.core)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- android/app/src/test/java/com/bsbarron/midschoolapp/ui/home/HomeViewModelTest.kt
+++ android/app/src/test/java/com/bsbarron/midschoolapp/ui/home/HomeViewModelTest.kt
@@ -1,15 +1,44 @@
 package com.bsbarron.midschoolapp.ui.home
 
+import android.app.Application
+import androidx.test.core.app.ApplicationProvider
+import com.bsbarron.midschoolapp.R
+import com.bsbarron.midschoolapp.MisSchoolApplication
+import com.bsbarron.midschoolapp.data.model.SchoolInfo
 import com.bsbarron.midschoolapp.data.repository.StudentInfo
 import com.bsbarron.midschoolapp.test.FakePreferencesRepository
 import com.bsbarron.midschoolapp.test.FakeSchoolRepository
-import com.bsbarron.midschoolapp.test.TestApplication
+import com.bsbarron.midschoolapp.ui.settings.SettingsViewModel
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.robolectric.annotation.Config
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
+@Config(application = MisSchoolApplication::class, sdk = [34])
 class HomeViewModelTest {
 
-    private val application = TestApplication()
+    @Test
+    fun home_refreshesHeaderAfterSettingsSave() = runBlocking {
+        val application = ApplicationProvider.getApplicationContext<Application>()
+        val initialSchool = SchoolInfo(
+            officeCode = "J10",
+            schoolCode = "1111111",
+            schoolName = "구미중학교",
+            schoolKind = "중학교"
+        )
+        val updatedSchool = SchoolInfo(
+            officeCode = "J10",
+            schoolCode = "2222222",
+            schoolName = "구미고등학교",
+            schoolKind = "고등학교"
+        )
 
     @Test
     fun `refreshHeader_updatesStudentInfoAfterSettingsChangedWithoutRecreatingViewModel`() {
