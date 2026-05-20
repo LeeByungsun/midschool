1fdc0518523691af2a2cd3f89b624042d79cb8c9
bslee <buggyani@hanmail.net>
2026-05-20 10:14:36 +0900

    omx(team): auto-checkpoint worker-2 [2]

---
+++ android/app/src/test/java/com/bsbarron/midschoolapp/MainActivityNavigationTest.kt
@@ -0,0 +1,25 @@
+package com.bsbarron.midschoolapp
+
+import android.view.View
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(application = MisSchoolApplication::class, sdk = [34])
+class MainActivityNavigationTest {
+
+    @Test
+    fun mealCardClickStartsMealActivity() {
+        val activity = Robolectric.buildActivity(MainActivity::class.java).setup().get()
+
+        activity.findViewById<View>(R.id.mealCard).performClick()
+
+        val nextIntent = shadowOf(activity).nextStartedActivity
+        assertEquals(MealActivity::class.java.name, nextIntent.component?.className)
+    }
+}
